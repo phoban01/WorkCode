a28d2dbf8ebc9708adcf9c88cbcb2c49cd482fa6
phoban01 <phoban01@gmail.com>
2014-10-27 15:33:26 +0000

    Added a comment about analytics

---
+++ xml2midi.py
@@ -0,0 +1,8 @@
+from music21 import *
+
+xml = converter.parse("/users/piarashoban/documents/schbavmasample.xml")
+
+# lots of scope for doing analytics in music21
+print xml.metadata.title
+
+xml.write(".midi","/users/piarashoban/documents/schubert.midi")git
