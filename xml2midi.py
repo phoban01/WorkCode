3c4da1fd4acd5d1b698ed71bc3019d4149ab1035
phoban01 <phoban01@gmail.com>
2014-10-28 09:14:51 +0000

    First working instance

--- xml2midi.py
+++ xml2midi.py
@@ -1,8 +1,16 @@
+import os
 from music21 import *
 
-xml = converter.parse("/users/piarashoban/documents/schbavmasample.xml")
+source = os.path.expanduser("~/documents/musXML/ryan-xml")
 
-# lots of scope for doing analytics in music21
-print xml.metadata.title
-
-xml.write(".midi","/users/piarashoban/documents/schubert.midi")git
+for mxml in [x for x in os.listdir(source) if x.endswith('.xml')]:
+	path = source + '/' +mxml
+	midipath = path.replace('ryan-xml','ryan-midi').replace('.xml','.midi')
+	parsed_xml = converter.parse(path)
+	print parsed_xml.metadata.title
+	for el in parsed_xml.recurse():
+		if 'Instrument' in el.classes: # or 'Piano'
+			el.activeSite.replace(el,instrument.Piano())
+		elif 'Dynamic' in el.classes:
+			el.activeSite.replace(el,dynamics.Dynamic('p'))
+	parsed_xml.write(".midi",midipath)
