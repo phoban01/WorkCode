78e705c10effabf9064a99cd140288bff66ff90b
phoban01 <phoban01@gmail.com>
2014-10-28 11:01:03 +0000

    Found problem with MelodyShape Error

--- xml2midi.py
+++ xml2midi.py
@@ -3,7 +3,7 @@ from music21 import *
 
 source = os.path.expanduser("~/documents/musXML/ryan-xml")
 
-for mxml in [x for x in os.listdir(source) if x.endswith('.xml')]:
+for mxml in [x for x in os.listdir(source)[:10] if x.endswith('.xml')]:
 	path = source + '/' +mxml
 	midipath = path.replace('ryan-xml','ryan-midi').replace('.xml','.midi')
 	parsed_xml = converter.parse(path)
@@ -13,4 +13,7 @@ for mxml in [x for x in os.listdir(source) if x.endswith('.xml')]:
 			el.activeSite.replace(el,instrument.Piano())
 		elif 'Dynamic' in el.classes:
 			el.activeSite.replace(el,dynamics.Dynamic('p'))
+	# problem isnt chords but grace notes... they occupy the same position... can
+	# we just strip them out?
 	parsed_xml.write(".midi",midipath)
+
